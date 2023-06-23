ddad108aae6ca53dd159312fd66d30b841fba32e
dk <kdi_2004@mail.ru>
2023-06-23 21:39:28 +0300

    тесты роболектрик

---
+++ app/src/test/java/com/example/tests/RobolectricTest.kt
@@ -0,0 +1,50 @@
+package com.example.tests
+
+import android.os.Build
+import androidx.fragment.app.Fragment
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import ru.dk.mydictionary.ui.MainActivity
+import ru.dk.mydictionary.ui.list.SearchListFragment
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.TIRAMISU])
+class RobolectricTest {
+
+    lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun fragment_AssertNotNull() {
+        val fragment: Fragment = SearchListFragment.newInstance()
+        scenario.onActivity {
+            it.supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_container, fragment)
+                .commitNow()
+            assertNotNull(fragment)
+        }
+    }
+}
